2f396834d40b46e96de4dac8ff46516b0cb85c65
mbashia <mbashiavictor@gmail.com>
2024-09-06 16:31:06 +0300

    create  search live folder

---
+++ lib/github_search_app_web/live/search_live/index.ex
@@ -0,0 +1,9 @@
+defmodule GithubSearchAppWeb.SearchLive.Index do
+    use GithubSearchAppWeb, :live_view
+  
+    def mount(_params, _session, socket) do
+      ## djdjdjd
+      {:ok, socket}
+    end
+  end
+  
