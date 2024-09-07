768f11f37559105b28dc125c283f9a2469690df1
mbashia <mbashiavictor@gmail.com>
2024-09-07 11:49:59 +0300

    create userprofile struct

---
+++ lib/github_search_app/user_profile.ex
@@ -0,0 +1,18 @@
+defmodule GithubSearchApp.UserProfile do
+  defstruct [
+    :avatar_url,
+    :bio,
+    :blog,
+    :company,
+    :created_at,
+    :email,
+    :followers,
+    :following,
+    :location,
+    :login,
+    :name,
+    :public_repos,
+    :twitter_username,
+    :profile_url
+  ]
+end
