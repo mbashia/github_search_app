a6faef5f3a4cadd8a66dccaab4841b394c233f5a
mbashia <mbashiavictor@gmail.com>
2024-09-07 11:50:16 +0300

    create default user

--- lib/github_search_app_web/live/search_live/index.ex
+++ lib/github_search_app_web/live/search_live/index.ex
@@ -5,6 +5,7 @@ defmodule GithubSearchAppWeb.SearchLive.Index do
     ## djdjdjd
     {:ok,
      socket
+     |> assign(:user, default_user())
      |> assign(:title, "dev finder")
      |> assign_new(:form, fn ->
        to_form(%{})
@@ -22,5 +23,24 @@ defmodule GithubSearchAppWeb.SearchLive.Index do
   def handle_event("search", %{"search" => search}, socket) do
     IO.inspect(search)
     {:noreply, socket}
+  end
+
+  def default_user() do
+    %GithubSearchApp.UserProfile{
+      avatar_url: "https://avatars.githubusercontent.com/u/96841734?v=4",
+      bio: "SOFTWARE ENGINEER!!",
+      blog: "https://www.mbashia.com",
+      company: "Mbashia Tech",
+      created_at: "2021-12-29T18:54:05Z",
+      email: nil,
+      followers: 18,
+      following: 47,
+      location: "Nairobi,Kenya",
+      login: "mbashia",
+      name: "mbashia",
+      public_repos: 67,
+      twitter_username: "VMbashia",
+      profile_url: "https://github.com/mbashia"
+    }
   end
 end
