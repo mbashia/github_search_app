269009565276f318fc916c0edc086193b2c3007d
mbashia <mbashiavictor@gmail.com>
2024-09-07 09:18:55 +0300

    add search input field and mix format

--- lib/github_search_app_web/live/search_live/index.ex
+++ lib/github_search_app_web/live/search_live/index.ex
@@ -1,9 +1,20 @@
 defmodule GithubSearchAppWeb.SearchLive.Index do
-    use GithubSearchAppWeb, :live_view
-  
-    def mount(_params, _session, socket) do
-      ## djdjdjd
-      {:ok, socket}
-    end
+  use GithubSearchAppWeb, :live_view
+  @impl true
+  def mount(_params, _session, socket) do
+    ## djdjdjd
+    {:ok,
+     socket
+     |> assign(:title, "dev finder")
+     |> assign_new(:form, fn ->
+       to_form(%{})
+     end)}
   end
-  
+
+  @impl true
+
+  def handle_event("validate", %{"search" => search}, socket) do
+    IO.inspect(search)
+    {:noreply, socket}
+  end
+end
--- test/github_search_app_web/controllers/error_json_test.exs
+++ test/github_search_app_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule GithubSearchAppWeb.ErrorJSONTest do
   use GithubSearchAppWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert GithubSearchAppWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert GithubSearchAppWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
